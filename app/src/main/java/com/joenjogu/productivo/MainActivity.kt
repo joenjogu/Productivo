d63f1093a4ef675ad4f8bb1fdccaf23b17c2effd
Joe Njogu <joenjogu97@gmail.com>
2020-09-26 12:36:16 +0300

    First commit

---
+++ app/src/main/java/com/joenjogu/productivo/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.joenjogu.productivo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        displayFragment(WorkFragment())
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.work_menu -> {
+                    displayFragment(WorkFragment())
+                    true
+                }
+                R.id.tasks_menu -> {
+                    displayFragment(TaskFragment())
+                    true
+                }
+                R.id.stats_menu -> {
+                    displayFragment(StatsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun displayFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}

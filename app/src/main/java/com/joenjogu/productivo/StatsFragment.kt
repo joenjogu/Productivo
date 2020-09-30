3efcd694e7f2070acf5c6986598c35f4524fcc87
Joe Njogu <joenjogu97@gmail.com>
2020-09-30 09:10:56 +0300

    Added a calendarView and its display logic for the stats fragment

--- app/src/main/java/com/joenjogu/productivo/StatsFragment.kt
+++ app/src/main/java/com/joenjogu/productivo/StatsFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import kotlinx.android.synthetic.main.fragment_stats.*
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
 
 class StatsFragment : Fragment() {
 
@@ -14,5 +22,35 @@ class StatsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+
+        stats_calendar.setup(firstMonth, lastMonth, firstDayOfWeek)
+        stats_calendar.scrollToMonth(currentMonth)
+
+
+        class DayViewContainer(view : View) : ViewContainer(view){
+            val textView = view.findViewById<TextView>(R.id.calendar_day_text)
+        }
+
+        stats_calendar.dayBinder = object : DayBinder<DayViewContainer> {
+
+            override fun create(view: View): DayViewContainer {
+                return DayViewContainer(view)
+            }
+
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+            }
+        }
+
+
     }
 }
